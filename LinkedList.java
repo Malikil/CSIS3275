802406eb1ce322defefb600b0d248b91058a294a
IAmArray <32501203+IAmArray@users.noreply.github.com>
2017-11-14 18:15:53 -0800

    Working on LinkedList, LLNode finished

---
+++ LinkedList.java
@@ -0,0 +1,27 @@
+package Server;
+
+public class LinkedList<T> {
+	private LLNode<T> first;
+	private LLNode<T> last;
+	
+	public LinkedList()
+	{
+		first = null;
+		last = null;
+	}
+	
+	public void add(T value)
+	{
+		if(isEmpty()==true)
+			first = new LLNode<T>(e)
+	}
+	public boolean isEmpty()
+	{
+		if(first.equals(last))
+			return true;
+		else
+			return false;
+	}
+	
+
+}
