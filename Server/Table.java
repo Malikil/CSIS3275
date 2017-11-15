4e613962bd53cbca4d45252703f1ed18b63bf454
Angelo Villadolid <villadolidangeloryan@gmail.com>
2017-11-14 18:02:08 -0800

    Added Table and Field

---
+++ Server/Table.java
@@ -0,0 +1,23 @@
+package Server;
+
+
+public class Table {
+	Field[] fields;
+	AVLTree<Entry> entries;
+		
+	public Table()
+	{
+		 entries = new AVLTree<Entry>();
+	}	
+	
+	public void addField(Field toAdd)
+	{
+		//Addfield to linkedlist
+	}
+	
+	public void rmvField(Field toRmv)
+	{
+		//field to linkedlist
+	}
+	
+}
