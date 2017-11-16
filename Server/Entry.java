15675df0016f5b08fafa128364451fb3cdb96a15
Malikil <eboy.coltt5@gmail.com>
2017-11-16 13:57:10 -0800

    Create custom ArrayList class
    Definitely Not ~~Blitzcrank~~ ArrayList

--- Server/Entry.java
+++ Server/Entry.java
@@ -1,32 +1,31 @@
 package Server;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
 @SuppressWarnings("rawtypes")
 public class Entry implements Comparable<Entry>, Serializable
 {
 	private static final long serialVersionUID = 481061902950995857L;
 	
-	private static int comparer;
+	private static int comparer = -1;
 	public static int getComparer() { return comparer; }
 	public static void setComparer(int fieldNumber) { comparer = fieldNumber; }
 	
-	private MyAL<Comparable> fields; //TODO: Need to change ArrayList to LinkedList 
 	private final int primaryKey;
+	private DefinitelyNotArrayList<Comparable> fields;
 	
 	public Entry(int key)
 	{
-		fields = new MyAL<Comparable>(); //TODO: Need to change ArrayList to LinkedList 
+		fields = new DefinitelyNotArrayList<Comparable>();
 		primaryKey = key;
 	}
 	
 	public Entry(int key, Comparable[] data)
 	{
-		fields = new MyAL<Comparable>(data.length);
+		fields = new DefinitelyNotArrayList<Comparable>(data.length);
 		for (Comparable d : data)
 			fields.add(d);
-		this.primaryKey = key;
+		primaryKey = key;
 	}
 	
 	public Comparable getField(int fieldNumber)
@@ -41,7 +40,7 @@ public class Entry implements Comparable<Entry>, Serializable
 	
 	public void addField(int fieldNumber, Comparable data)
 	{
-		fields.add(fieldNumber, data);
+		fields.add(data, fieldNumber);
 	}
 	
 	public void addField(Comparable data)
