40c44b70f8b9a9cd47c7fc0712f705f590127df9
Malikil <eboy.coltt5@gmail.com>
2017-11-28 12:07:45 -0800

    Fix reconstructing/sorting an empty tree
    No more NullPointerException

--- Server/AVLTree.java
+++ Server/AVLTree.java
@@ -235,7 +235,8 @@ public class AVLTree<T extends Comparable<T>> implements Serializable
 	public AVLTree<T> reconstructTree()
 	{
 		AVLTree<T> temp = new AVLTree<>();
-		reconstructNode(base, temp);
+		if (base != null)
+			reconstructNode(base, temp);
 		return temp;
 	}
 	
@@ -251,26 +252,27 @@ public class AVLTree<T extends Comparable<T>> implements Serializable
 	public AVLTree<T> getRange(T value, String mode)
 	{
 		AVLTree<T> temp = new AVLTree<T>();
-		switch (mode)
-		{
-		case "<":
-			lessThanNode(base, value, temp);
-			break;
-		case "<=":
-			lessThanNode(base, value, temp);
-			equalToNode(base, value, temp);
-			break;
-		case "=":
-			equalToNode(base, value, temp);
-			break;
-		case ">=":
-			greaterThanNode(base, value, temp);
-			equalToNode(base, value, temp);
-			break;
-		case ">":
-			greaterThanNode(base, value, temp);
-			break;
-		}
+		if (base != null)
+			switch (mode)
+			{
+			case "<":
+				lessThanNode(base, value, temp);
+				break;
+			case "<=":
+				lessThanNode(base, value, temp);
+				equalToNode(base, value, temp);
+				break;
+			case "=":
+				equalToNode(base, value, temp);
+				break;
+			case ">=":
+				greaterThanNode(base, value, temp);
+				equalToNode(base, value, temp);
+				break;
+			case ">":
+				greaterThanNode(base, value, temp);
+				break;
+			}
 		return temp;
 	}
 	
